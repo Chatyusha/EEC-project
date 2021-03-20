fd7eb6836f13f4392d69e9d5e9b1494a7f2b3d5d
SecretCat <luckyhiroaki@icloud.com>
2021-03-20 01:27:43 +0000

    update pyenv installer

--- pkg/pyenv/install.sh
+++ pkg/pyenv/install.sh
@@ -15,13 +15,6 @@ git
 
 git clone https://github.com/pyenv/pyenv.git "$LIBRARY/Python"
 
-echo "Delete 'STATE_PYENV=0' and Write 'STATE_PYENV=1' on your zhsrc"
-echo "===== zshrc ====="
-echo "<<< STATE_PYENV=0"
-echo ">>> STATE_PYENV=1"
-echo "================="
-echo "And run 'source ~/.zshrc'"
-echo "How to install python"
-echo "Example use Python version 3.8.0"
-echo "run \`pyenv install 3.6.10\`"
+echo "Enable \`STATE_PYENV\` and run \`source ~/..zshrc\`."
+echo "Run \`pyenv install <python version>\`, for example, \`pyenv install 3.8.0\`."
 
