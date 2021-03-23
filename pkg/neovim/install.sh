badc13c488b51c0f79d7ad2541396c7d9a41242e
SecretCat <luckyhiroaki@icloud.com>
2021-03-23 05:40:37 +0000

    make installer for Neovim

--- pkg/neovim/install.sh
+++ pkg/neovim/install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -ex
 
-if [ ! -d "$HOME_LIBRARY" ]
+if [ ! -d "$HOME_LIBRARY/bin" ]; then
+	mkdir -p "$HOME_LIBRARY/bin"
+fi
+
+if [ ! -d "$HOME_LIBRARY/work/neovim" ];then
+	mkdir -p "$HOME_LIBRARY/work/neovim"
+fi
+cd "$HOME_LIBRARY/work/neovim"
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
+
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+ln -s $(pwd)/squashfs-root/AppRun "$HOME_LIBRARY/bin/nvim"
